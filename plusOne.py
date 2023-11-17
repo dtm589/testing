f261ba83c219e5e8348814d16eacfbb4a547d9b7
Derek Marchese <derekmarchese1@gmail.com>
2023-11-17 16:33:32 -0500

    plus one plus one

--- plusOne.py
+++ plusOne.py
@@ -13,3 +13,17 @@ class Solution:
         temp = str(int(string) +1)
 
         return [int(temp[i]) for i in range(len(temp))]
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n = len(digits)
+
+        for i in range(n-1, -1, -1):
+            if(digits[i] == 9):
+                digits[i] = 0
+            else:
+                digits[i] = digits[i] + 1
+                return digits
+        
+        digits.insert(0,1)
+        return digits
