3194618a2ee6916fec3b8b2626be81fa3cf3072b
Derek Marchese <derekmarchese1@gmail.com>
2023-11-18 15:04:42 -0500

    met the target

---
+++ metTheTarget.py
@@ -0,0 +1,17 @@
+'''
+There are n employees in a company, numbered from 0 to n - 1. Each employee i has worked for hours[i] hours in the company.
+
+The company requires each employee to work for at least target hours.
+
+You are given a 0-indexed array of non-negative integers hours of length n and a non-negative integer target.
+
+Return the integer denoting the number of employees who worked at least target hours.
+'''
+
+class Solution:
+    def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
+        output = 0
+        for num in hours:
+            if num >= target:
+                output += 1
+        return output
