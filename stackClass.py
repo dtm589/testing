f9a43f53ac747e9003089f9fcf8d56929804da96
Derek Marchese <derekmarchese1@gmail.com>
2023-12-11 12:52:29 -0500

    class stack

---
+++ stackClass.py
@@ -0,0 +1,24 @@
+'''
+Implementing a stack class
+'''
+
+
+class Stack:
+    def __init__(self):
+        self.arrows = []
+
+    def push(self, arrow):
+        self.arrows.append(arrow)
+
+    def pop(self):
+        if self.arrows:
+            return self.arrows.pop()
+        return None
+
+    def peek(self):
+        if self.arrows:
+            return self.arrows[-1]
+        return None
+
+    def size(self):
+        return len(self.arrows)
