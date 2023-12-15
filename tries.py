4faaae7d2b3a00a8b307e2751492e9bb1e4560d9
Derek Marchese <derekmarchese1@gmail.com>
2023-12-15 18:26:26 -0500

    tries

--- tries.py
+++ tries.py
@@ -6,6 +6,18 @@ class Trie:
                 current[char] = {}
             current = current[char]
         current[self.end_symbol] = True
+        
+    def exists(self, word):
+        current = self.root
+        for letter in word:
+            if letter in current:
+                current = current[letter]
+            else:
+                return False
+        if self.end_symbol in current:
+            return True
+        else:
+            return False
 
 
     def __init__(self):
