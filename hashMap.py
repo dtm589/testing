41eedbf5e62afc6de71bcf97c346bc6c0153e111
Derek Marchese <derekmarchese1@gmail.com>
2023-12-15 18:01:56 -0500

    hash map

--- hashMap.py
+++ hashMap.py
@@ -15,10 +15,11 @@ class HashMap:
             if v != None:
                 buckets.append(v)
         return str(buckets)
-
+    
     def insert(self, key, value):
+        self.resize()
         index = self.key_to_index(key)
-        self.hashmap[index] = (key,value)
+        self.hashmap[index] = (key, value)
         
     def get(self, key):
         index = self.key_to_index(key)
@@ -26,3 +27,24 @@ class HashMap:
             return self.hashmap[index][1]
         else:
             raise Exception("sorry, key not found")
+        
+    def resize(self):
+        if len(self.hashmap) == 0:
+            self.hashmap = [None]
+        load = self.current_load()
+        if load >= .05:
+            new_hashmap = [None]*len(self.hashmap)*10
+            temp_hashmap = self.hashmap
+            self.hashmap = new_hashmap
+            for bucket in temp_hashmap:
+                if bucket != None:
+                    self.insert(bucket[0], bucket[1])
+                    
+    def current_load(self):
+        if len(self.hashmap) == 0:
+            return 1
+        buckets = 0
+        for bucket in self.hashmap:
+            if bucket != None:
+                buckets += 1
+        return buckets / len(self.hashmap)
