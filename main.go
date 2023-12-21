bec466e34e4c55bbdb4493a77c785cd22ce80df7
Derek Marchese <derekmarchese1@gmail.com>
2023-12-21 10:51:15 -0500

    ignoring values with _

--- main.go
+++ main.go
@@ -64,3 +64,13 @@ func main() {
 func incrementSends(sendsSoFar, sendsToAdd int) int {
 	return sendsSoFar + sendsToAdd
 }
+
+
+func main() {
+	firstName, _ := getNames()
+	fmt.Println("Welcome to Textio,", firstName)
+}
+
+func getNames() (string, string) {
+	return "John", "Doe"
+}
