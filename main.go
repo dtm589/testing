228b7f31fa331f6954d78eb3e7e3f3a48ed2d7e3
Derek Marchese <derekmarchese1@gmail.com>
2023-12-21 10:37:10 -0500

    go

--- main.go
+++ main.go
@@ -8,4 +8,43 @@ func main() {
 	var hasPermission bool
 	var username string
 	fmt.Printf("%v %f %v %q\n", smsSendingLimit, costPerSMS, hasPermission, username)
+
+
+	accountAge := 2.6
+	// create a new "accountAgeInt" here
+	// it should be the result of casting "accountAge" to an integer
+	accountAgeInt := int(accountAge)
+
+
+	averageOpenRate, displayMessage := .23, "is the average open rate of your messages"
+	fmt.Println(averageOpenRate, displayMessage)
+
+
+	const premiumPlanName = "Premium Plan"
+	const basicPlanName = "Basic Plan"
+	fmt.Println("plan:", premiumPlanName)
+	fmt.Println("plan:", basicPlanName)
+
+
+	const secondsInMinute = 60
+	const minutesInHour = 60
+	const secondsInHour = secondsInMinute * minutesInHour
+	fmt.Println("number of seconds in an hour:", secondsInHour)
+
+
+	const name = "Saul Goodman"
+	const openRate = 30.5
+	msg := fmt.Sprintf("Hi %s, your open rate is %.1f percent\n", name, openRate)
+	fmt.Print(msg)
+
+
+	messageLen := 10
+	maxMessageLen := 20
+	fmt.Println("Trying to send a message of length:", messageLen, "and a max length of:", maxMessageLen)
+	if messageLen <= maxMessageLen {
+		fmt.Println("Message sent")
+	} else {
+		fmt.Println("Message not sent")
+	}
+
 }
