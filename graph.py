b6ad497e697d210f8263d17dc9bed2b58572d136
Derek Marchese <derekmarchese1@gmail.com>
2023-12-20 23:10:05 -0500

    Depth First Search

--- graph.py
+++ graph.py
@@ -41,3 +41,15 @@ class Graph:
                 if neigbor not in visited and neigbor not in to_visit:
                     to_visit.append(neigbor)
         return visited
+    
+    def depth_first_search(self, start_vertex):
+        visited = []
+        self.depth_first_search_r(visited, start_vertex)
+        return visited
+
+    def depth_first_search_r(self, visited, current_vertex):
+        visited.append(current_vertex)
+        sorted_list = sorted(self.graph[current_vertex])
+        for neighbor in sorted_list:
+            if neighbor not in visited:
+                self.depth_first_search_r(visited, neighbor)
