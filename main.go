6a414c231c178849d8009a6d5124081d29626bec
Derek Marchese <derekmarchese1@gmail.com>
2023-12-21 10:53:52 -0500

    named return values

--- main.go
+++ main.go
@@ -74,3 +74,20 @@ func main() {
 func getNames() (string, string) {
 	return "John", "Doe"
 }
+
+
+func yearsUntilEvents(age int) (yearsUntilAdult, yearsUntilDrinking, yearsUntilCarRental int) {
+	yearsUntilAdult = 18 - age
+	if yearsUntilAdult < 0 {
+		yearsUntilAdult = 0
+	}
+	yearsUntilDrinking = 21 - age
+	if yearsUntilDrinking < 0 {
+		yearsUntilDrinking = 0
+	}
+	yearsUntilCarRental = 25 - age
+	if yearsUntilCarRental < 0 {
+		yearsUntilCarRental = 0
+	}
+	return
+}
