b41ea52ce35ecfde509b6f15f1a8fbdfe62e3746
Derek Marchese <derekmarchese1@gmail.com>
2023-12-30 17:50:09 -0500

    div and nondiv

---
+++ divAndNonDiv.py
@@ -0,0 +1,21 @@
+'''
+You are given positive integers n and m.
+
+Define two integers, num1 and num2, as follows:
+
+num1: The sum of all integers in the range [1, n] that are not divisible by m.
+num2: The sum of all integers in the range [1, n] that are divisible by m.
+Return the integer num1 - num2.
+'''
+
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        num1 = 0
+        num2 = 0
+        for i in range(1, n + 1):
+            if i % m == 0:
+                num2 += i
+            else:
+                num1+= i
+
+        return num1 - num2
