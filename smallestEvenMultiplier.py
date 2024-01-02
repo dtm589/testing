86692e2b207f9b615f256790fcfba4224e709045
Derek Marchese <derekmarchese1@gmail.com>
2024-01-01 23:54:54 -0500

    smallest even mulitplier

---
+++ smallestEvenMultiplier.py
@@ -0,0 +1,10 @@
+'''
+Given a positive integer n, return the smallest positive integer that is a multiple of both 2 and n.
+'''
+
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+        if n % 2 == 0:
+            return n
+        else:
+            return n * 2
