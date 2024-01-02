a376d415b933ba81500852998aa55d83407e256f
Derek Marchese <derekmarchese1@gmail.com>
2024-01-02 00:11:19 -0500

    running sum

---
+++ runningSum.py
@@ -0,0 +1,16 @@
+'''
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+'''
+
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        output = []
+        total = 0
+        for number in nums:
+            total += number
+            output.append(total)
+
+        return output
